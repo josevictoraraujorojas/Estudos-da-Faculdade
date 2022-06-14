f770f44a5a87fab380132ea78ae74c8a091e7078
josevictoraraujorojas <josevictor.rojas@hotmail.com>
2022-06-14 12:23:12 -0300

    mudança no ex6 da lista 3

--- src/Lista04/Ex6.java
+++ src/Lista04/Ex6.java
@@ -2,14 +2,15 @@ package Lista04;
 
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Ex6 {
-    public static void main(String[] args) {
-//        Questão 6 Escreva um programa em Java que armazene 10 números aleatórios em um vetor.
+    public static void main(String[] args) throws InterruptedException {
+        //        Questão 6 Escreva um programa em Java que armazene 10 números aleatórios em um vetor.
 //        Informe ao usuárioque os números estão sendo atribuídos ao vetor e diga quais são esses números.
 //        Em seguida, informe as seguintes opções para o usuário escolher o que quer ver:
 
-        Random aleatorio= new Random();
+        Random aleatorio = new Random();
         Scanner ler = new Scanner(System.in);
         int[] numeros = new int[10];
         int[] duplicado = new int[10];
@@ -17,22 +18,31 @@ public class Ex6 {
         String novamente;
         int multiplo;
 
-        for (int i = 0; i < 10; i++)
-        {
-            numeros[i] = aleatorio.nextInt(-10,10);
+        for (int i = 0; i < 10; i++) {
+            numeros[i] = aleatorio.nextInt(-10, 11);
+            System.out.println(numeros[i]);
+            TimeUnit.SECONDS.sleep(1);
         }
         do {
             System.out.println("este é o vetor: " + "{" + numeros[0] + "," + numeros[1] + "," + numeros[2] + "," + numeros[3] + "," + numeros[4] + "," + numeros[5] + "," + numeros[6] + "," + numeros[7] + "," + numeros[8] + "," + numeros[9] + "}");
+            TimeUnit.SECONDS.sleep(2);
             System.out.println("o que voce quer saber sobre o vetor");
+            TimeUnit.SECONDS.sleep(2);
             System.out.println("se voce quiser saber a quantidade dos numeros primos escreva 'primo'");
+            TimeUnit.SECONDS.sleep(2);
             System.out.println("se voce quiser saber a quantidade de numeros positivo e negativos do vetor ecreva 'positivonegativo'");
+            TimeUnit.SECONDS.sleep(2);
             System.out.println("se voce quiser saber a quantidade de numeros multiplos ecreva 'multiplo'");
+            TimeUnit.SECONDS.sleep(2);
             System.out.println("se voce quiser saber o maior e o menor numero do vetor ecreva 'maiormenor'");
+            TimeUnit.SECONDS.sleep(2);
             System.out.println("se voce quiser saber a quantidade de numeros pares e impares do vetor ecreva 'parimpar'");
+            TimeUnit.SECONDS.sleep(2);
             System.out.println("se voce quiser saber os numeros duplicados do vetor ecreva 'duplicado'");
+            TimeUnit.SECONDS.sleep(2);
             System.out.println("informe aqui:");
             resposta = ler.next();
-            while (!(resposta.equals("primo") || resposta.equals("positivonegativo") || resposta.equals("multiplo") || resposta.equals("maiormenor") ||  resposta.equals("parimpar") || resposta.equals("duplicado"))) {
+            while (!(resposta.equals("primo") || resposta.equals("positivonegativo") || resposta.equals("multiplo") || resposta.equals("maiormenor") || resposta.equals("parimpar") || resposta.equals("duplicado"))) {
                 System.out.println("resposta invalida");
                 System.out.println("responda novamente");
                 resposta = ler.next();
@@ -48,101 +58,99 @@ public class Ex6 {
                     verificaMultiplo(numeros, multiplo);
                 }
                 case "maiormenor" -> verificaMaiorMenor(numeros);
-                case "duplicado" -> verificaDuplicado(numeros,duplicado);
+                case "duplicado" -> verificaDuplicado(numeros, duplicado);
 
             }
 
             System.out.println("voce quer saber mais cosisas sobre o vetor");
-            System.out.println("Voce quer saber mais sobre o vetor se sim digite 'sim'");
-            novamente= ler.next();
+            TimeUnit.SECONDS.sleep(1);
+            System.out.println("Voce quer saber mais sobre o vetor se sim digite 'sim' se nao digite 'nao'");
+            novamente = ler.next();
+            TimeUnit.SECONDS.sleep(1);
+            while (!(novamente.equals("sim") || novamente.equals("nao"))) {
+                System.out.println("respota ivalida digite novamente:");
+                novamente = ler.next();
+                TimeUnit.SECONDS.sleep(1);
+            }
 
-        }while (novamente.equals("sim"));
+        } while (novamente.equals("sim"));
     }
-    public static void verificaParImpar(int[] x){
-        int countPar=0;
-        int countImpar=0;
-        for (int i=0;i<10;i++)
-        {
-            if (x[i] % 2 == 0)
-            {
+
+    public static void verificaParImpar(int[] x) {
+        int countPar = 0;
+        int countImpar = 0;
+        for (int i = 0; i < 10; i++) {
+            if (x[i] % 2 == 0) {
                 countPar++;
-            } else
-            {
+            } else {
                 countImpar++;
             }
         }
-        System.out.println("quantidade de numeros impares do vetor: "+countImpar);
-        System.out.println("quantidade de numeros pares do vetor: "+countPar);
+        System.out.println("quantidade de numeros impares do vetor: " + countImpar);
+        System.out.println("quantidade de numeros pares do vetor: " + countPar);
     }
-    public static void verificaPositivoNegativo(int[] x){
-        int countPositivo=0;
-        int countNegativo=0;
 
-        for (int i=0;i<10;i++)
-        {
-            if (x[i] > 0)
-            {
+    public static void verificaPositivoNegativo(int[] x) {
+        int countPositivo = 0;
+        int countNegativo = 0;
+
+        for (int i = 0; i < 10; i++) {
+            if (x[i] > 0) {
                 countPositivo++;
-            } else
-            {
+            } else {
                 countNegativo++;
             }
         }
-        System.out.println("quantidade de numeros positivos do vetor: "+countPositivo);
-        System.out.println("quantidade de numeros negativos do vetor: "+countNegativo);
+        System.out.println("quantidade de numeros positivos do vetor: " + countPositivo);
+        System.out.println("quantidade de numeros negativos do vetor: " + countNegativo);
     }
-    public static void verificaMaiorMenor(int[] x){
-        int maior= x[0], menor= x[0];
 
-        for (int i=1;i<9;i++){
-            if (x[i]>maior)
-            {
-                maior=x[i];
+    public static void verificaMaiorMenor(int[] x) {
+        int maior = x[0], menor = x[0];
+
+        for (int i = 1; i < 9; i++) {
+            if (x[i] > maior) {
+                maior = x[i];
             }
-            if (x[i]<menor)
-            {
-                menor=x[i];
+            if (x[i] < menor) {
+                menor = x[i];
             }
         }
-        System.out.println("este é o maior numero do vetor: "+maior);
-        System.out.println("este é o menore numero do vetor: "+menor);
+        System.out.println("este é o maior numero do vetor: " + maior);
+        System.out.println("este é o menore numero do vetor: " + menor);
     }
-    public static void verificaMultiplo(int[] x,int y){
-        int countMultiplo=0;
-        for (int i=0;i<10;i++)
-        {
-            if (x[i] % y == 0)
-            {
+
+    public static void verificaMultiplo(int[] x, int y) {
+        int countMultiplo = 0;
+        for (int i = 0; i < 10; i++) {
+            if (x[i] % y == 0) {
                 countMultiplo++;
             }
         }
-        System.out.println("qauntidade de multiplos do numero "+y+" : "+countMultiplo);
+        System.out.println("qauntidade de multiplos do numero " + y + " : " + countMultiplo);
     }
-    public static void verificaPrimo(int[] x){
 
-        int countPrimo=0;
-        for (int i=0;i<10;i++)
-        {
-            int count=0;
-            for (int j = x[i]; j>=1; j--)
-            {
-                if (x[i]%j==0)
-                {
+    public static void verificaPrimo(int[] x) {
+
+        int countPrimo = 0;
+        for (int i = 0; i < 10; i++) {
+            int count = 0;
+            for (int j = x[i]; j >= 1; j--) {
+                if (x[i] % j == 0) {
                     count++;
                 }
             }
-            if (count == 2)
-            {
+            if (count == 2) {
                 countPrimo++;
             }
         }
-        System.out.println("quantidade de numeros primos no vetor: "+countPrimo);
+        System.out.println("quantidade de numeros primos no vetor: " + countPrimo);
     }
-    public static void verificaDuplicado(int[] x,int[] y){
-        for (int i=0;i<10;i++)
-        {
+
+    public static void verificaDuplicado(int[] x, int[] y) {
+        for (int i = 0; i < 10; i++) {
             ++y[x[i]];
-            System.out.println("numero "+x[i]+" foi repetido "+y[i]);
+            System.out.println("numero " + x[i] + " foi repetido " + y[i]);
         }
     }
 }
