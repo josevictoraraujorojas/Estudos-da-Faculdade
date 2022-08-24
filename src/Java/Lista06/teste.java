5c0b408ae308e994a279543c7dc8b628858ee72a
Jose <kaneki1212112121@gmail.com>
2022-08-24 17:11:57 -0300

    beta jogo naval

--- src/Java/Lista06/teste.java
+++ src/Java/Lista06/teste.java
@@ -2,14 +2,22 @@ package Java.Lista06;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class teste {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner ler = new Scanner(System.in);
         String[][] tabuleiro = criandoTabuleiro();
+        String[][] jogo = criandoTabuleiro();
         int[] verificacao = new int[1];
+        int linha;
+        int coluna;
+        int barco=0;
+        int jogada=0;
 
 
-        //
+
+        //o laço roda ate que se cria 9 "/"
         do {
             //zera o tabuleiro caso ele passe de 9 "/"
             tabuleiro = criandoTabuleiro();
@@ -22,8 +30,65 @@ public class teste {
         }while (verificacao[0]!=1);
         imprimi(tabuleiro);
 
+        //menu do jogo
+        System.out.println("Você está jogando batalha naval");
+        System.out.println("Neste jogo você tem que afundar tres navios");
+        imprimi(jogo);
 
+        while (barco!=9) {
+            jogada++;
+            Thread.sleep(1000);
+            System.out.println("Jogada: " + jogada);
+            Thread.sleep(1000);
+            System.out.println("escolha uma coordenada para jogar a bomba");
+            linha = ler.nextInt();
+            coluna = ler.nextInt();
+            linha -= 1;
+            coluna -= 1;
+
+            while (!diferentesJogadas(linha, coluna, jogo)) {
+                System.out.println("Jogada ja feita escolha outra jogada");
+                System.out.println("Escolha a linha e a coluna ");
+                linha = ler.nextInt();
+                coluna = ler.nextInt();
+                linha -= 1;
+                coluna -= 1;
+            }
+
+
+            if (verificaBarco(linha, coluna, tabuleiro)) {
+                barco++;
+                Thread.sleep(1000);
+                System.out.println("embarcação destruída");
+                Thread.sleep(1000);
+                System.out.println("total de barcos encontrados: " + barco);
+                Thread.sleep(1000);
+                mostrabarco(linha, coluna, jogo);
+
+
+            } else {
+                escolhebomba(linha, coluna, jogo);
+                Thread.sleep(1000);
+                System.out.println("Nenhum barco encontrado");
+            }
+            Thread.sleep(1000);
+            imprimi(jogo);
+        }
+        System.out.println("Parabens capitão voce destruiu todos os navios");
     }
+    public static boolean diferentesJogadas(int f, int c, String[][] x) {
+        return x[f][c].equals("-");
+    }
+    public static boolean verificaBarco(int f, int c, String[][] x) {
+        return x[f][c].equals("/");
+    }
+    public static void escolhebomba (int f,int c,String[][] x) {
+        x[f][c]="*";
+    }
+    public static void mostrabarco (int f,int c,String[][] x) {
+        x[f][c]="/";
+    }
+
     public static void tresNavios (String[][] x,int[] y){
         int count1=0;
 
@@ -39,45 +104,47 @@ public class teste {
         }
         }
 
-        //cria os navios com 3 indicies de dist�ncia, fiz isso analizando as combina��es possiveis
+        //cria os navios com 3 indicies de distância, fiz isso analizando as combinações possiveis
     public static void criaBarcos (String[][] x){
         Random r = new Random();
         int posssibilidadeLinha = r.nextInt( 1,7);
         int linharandom= r.nextInt(5);
         int colunarandom= r.nextInt(5);
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 5; i++) {
             //linha possibilidade 1
-            if (posssibilidadeLinha == 1 ) {
+            if (posssibilidadeLinha == 1 && i<3) {
                 x[linharandom][i] = "/";
             }
+
             //coluna possibilidade 2
-            if (posssibilidadeLinha == 2) {
-                x[i][colunarandom] = "/";
-            }
-        }
-        for (int i = 1; i < 4; i++) {
-            //linha possibilidade 3
-            if (posssibilidadeLinha == 3) {
-                x[linharandom][i] = "/";
-            }
-            //coluna possibilidade 4
-            if (posssibilidadeLinha == 4) {
-                x[i][colunarandom] = "/";
-            }
-        }
-        for (int i = 2; i < 5; i++) {
-            //linha possibilidade 5
-            if (posssibilidadeLinha == 5) {
-                x[linharandom][i] = "/";
-            }
-            //coluna possibilidade 6
-            if (posssibilidadeLinha == 6) {
+            if (posssibilidadeLinha == 2 && i<3) {
                 x[i][colunarandom] = "/";
             }
 
+            //linha possibilidade 3
+            if (posssibilidadeLinha == 3 && i>0 && i<4) {
+                x[linharandom][i] = "/";
+            }
+
+            //coluna possibilidade 4
+            if (posssibilidadeLinha == 4 && i>0 && i<4) {
+                x[i][colunarandom] = "/";
+            }
+
+            //linha possibilidade 5
+            if (posssibilidadeLinha == 5 && i>1) {
+                x[linharandom][i] = "/";
+            }
+
+            //coluna possibilidade 6
+            if (posssibilidadeLinha == 6 && i>1) {
+                x[i][colunarandom] = "/";
+            }
         }
     }
+
+    //cria o tabuleiro
     public static String[][] criandoTabuleiro(){
         String[][] tabuleiro = new String[5][5];
         for (int i = 0; i < 5; i++) {
@@ -85,6 +152,8 @@ public class teste {
         }
         return tabuleiro;
     }
+
+    //imprimi o tabuleiro
     public static void imprimi(String[][] x) {
         for (int i = 0; i < x.length; i++) {
             for (int j = 0; j < x[0].length; j++) {
