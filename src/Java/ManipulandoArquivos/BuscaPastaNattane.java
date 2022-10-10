f781ea38500598ee7158e9189b064a818b39d50c
Jose <kaneki1212112121@gmail.com>
2022-10-10 16:11:04 -0300

    programa de busca de arquivos

--- src/Java/ManipulandoArquivos/BuscaPastaNattane.java
+++ src/Java/ManipulandoArquivos/BuscaPastaNattane.java
@@ -5,17 +5,30 @@ import java.util.Scanner;
 
 public class BuscaPastaNattane {
     public static void main(String[] args) {
-        Scanner ler = new Scanner(System.in);
-        analizandoPasta(ler.nextLine());
-    }
-    public static void analizandoPasta(String caminhoPasta){
-        File pasta = new File(caminhoPasta);
-        String[] arquivos = pasta.list();
-
-        for (String copia: arquivos) {
-            if (copia.contains("exe")){
-                System.out.println(copia);
+        while (true) {
+            try {
+                System.out.println("escreva o caminho para a pasta:");
+                Scanner ler = new Scanner(System.in);
+                String caminho= ler.nextLine();
+                System.out.println("escreva o tipo de arquivo que vc quer encontrar e saber a quantidade: ");
+                String tipoDeArquivo= ler.nextLine();
+                analizandoPasta(caminho,tipoDeArquivo);
+                break;
+            } catch (NullPointerException e) {
+                System.out.println("caminho nao encontrado");
             }
         }
+    }
+    public static void analizandoPasta(String caminhoPasta,String tipoDeArquivo){
+        File pasta = new File(caminhoPasta);
+        String[] arquivo = pasta.list();
+        int quantidadeDeArquivos=0;
+        for (String copia: arquivo) {
+            if (copia.contains(tipoDeArquivo)){
+                System.out.println(copia);
+                quantidadeDeArquivos++;
+            }
+        }
+        System.out.printf("\n quantidades de arquivos %s encontrados: %d", tipoDeArquivo, quantidadeDeArquivos);
     }
 }
