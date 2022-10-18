ec94f9ad7348d48317240fe3ff8c63affd109ef7
Jose <kaneki1212112121@gmail.com>
2022-10-17 22:35:17 -0300

    lista

--- src/Java/ManipulandoArquivos/Ex02.java
+++ src/Java/ManipulandoArquivos/Ex02.java
@@ -3,6 +3,7 @@ package Java.ManipulandoArquivos;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Ex02 {
@@ -11,14 +12,30 @@ public class Ex02 {
            Scanner ler = new Scanner(System.in);
            BufferedWriter armazenaSenha = new BufferedWriter(new FileWriter("BancoDeDadosSenha.txt", true));
            BufferedWriter armazenalogin = new BufferedWriter(new FileWriter("BancoDeDadosLogin.txt", true));
+
            System.out.println("Informe seu login:");
            String login = ler.nextLine();
-           while (verificacaoLogin(login)) {
-               System.out.println("login invalido");
+           System.out.println("informe a sua senha:");
+           String senha = ler.nextLine();
+           while (login.equals(senha)) {
+               System.out.println("Sua senha é a mesma que o seu login,tente novamente");
                System.out.println("Informe seu login:");
-               login = ler.nextLine();
+                login = ler.nextLine();
+               System.out.println("informe a sua senha:");
+               senha = ler.nextLine();
            }
+               while (verificacaoLogin(login)) {
+                   System.out.println("login invalido");
+                   System.out.println("Informe seu login:");
+                   login = ler.nextLine();
+               }
+               while (verificacaoSenha(senha)) {
+                   System.out.println("Senha invalida");
+                   System.out.println("Informe sua senha:");
+                   senha = ler.nextLine();
+               }
            armazenalogin.write(login+"\n");
+           armazenaSenha.write(criptografia(senha)+"\n");
            armazenalogin.close();
            armazenaSenha.close();
        }catch (IOException e){
@@ -47,5 +64,48 @@ public class Ex02 {
             }
         }
         return verificacao;
+    }
+    public static boolean verificacaoSenha(String senha){
+        boolean verificacao = false;
+        int contadorDeNumeros=0;
+        int contadorDeLetrasMaiusculas=0;
+        int contadorDeLetrasMinusculas=0;
+        for (char letra:senha.toCharArray()) {
+            if (!Character.isUpperCase(letra))
+                contadorDeLetrasMaiusculas++;
+
+            if (!Character.isLowerCase(letra))
+                contadorDeLetrasMinusculas++;
+
+            if (!Character.isDigit(letra))
+                contadorDeNumeros++;
+
+            if (Character.isSpaceChar(letra))
+            {
+                verificacao =true;
+                break;
+            }else if (!(senha.length()>=8&&senha.length()<=16))
+            {
+                verificacao =true;
+                break;
+            }
+            else if (contadorDeLetrasMaiusculas == senha.length()||contadorDeLetrasMinusculas == senha.length())
+                verificacao =true;
+
+            else if (contadorDeNumeros == senha.length())
+                verificacao = true;
+        }
+        return verificacao;
+    }
+    public static boolean verificacaoSenha2(String senha){
+        boolean temNumeroELetra=senha.matches("\\\\w\\\\d");
+    }
+    public static StringBuilder criptografia(String senha){
+        StringBuilder codificacao = new StringBuilder();
+        for (int i = 0; i < senha.length() ; i++) {
+            codificacao.append(senha.codePointAt(i));
+        }
+
+        return codificacao;
     }
 }
