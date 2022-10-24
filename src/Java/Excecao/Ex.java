69b654e1a35175c726395ddca8a23a89389bbcaa
josevictoraraujorojas <kaneki1212112121@gmail.com>
2022-10-24 10:28:13 -0300

    EX02

--- src/Java/Excecao/Ex.java
+++ src/Java/Excecao/Ex.java
@@ -7,15 +7,16 @@ import java.util.Scanner;
 public class Ex {
     public static void main(String[] args) {
         try {
-            int a=0;
-            int b=3;
-            int c=5;
-
-            a=a+b/3;
-            b=b/a;
-            System.out.println(b);
-        }catch (ArithmeticException e){
-            System.out.println("nao se pode dividir por zero");
+            int x=5;
+            int y=3;
+            int[] arrway = new int[x*y/5];
+            System.out.println(arrway[y]);
+            System.out.print("BOB");
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.print("Unicórnio");
+        }finally {
+            System.out.print("flutuante");
         }
+
     }
 }
