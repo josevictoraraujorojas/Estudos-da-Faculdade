347590a3d11fec9cee1624cbd68b9f3be1a8c1d8
Jose <kaneki1212112121@gmail.com>
2022-11-15 12:47:18 -0300

    recursão

--- src/Java/Recursao/Palindromo.java
+++ src/Java/Recursao/Palindromo.java
@@ -1,19 +1,23 @@
 package Java.Recursao;
 
 public class Palindromo {
+    public static int x =0;
     public static void main(String[] args) {
-        String palavra = "amor é roma";
-        if (verificaPalindromo(palavra,palavra.length()-1)){
-            System.out.printf("A palavra \"%s\" é um palindromo",palavra);
-        }else {
-            System.out.printf("A palavra \"%s\" não é um palindromo",palavra);
-        }
+        String palavra = "lava esse aval";
 
+        if (verificaPalindromo(palavra,palavra.length()-1)){
+            System.out.printf("a palavra \"%s\" e um palindromo",palavra);
+        }else {
+            System.out.printf("a palavra \"%s\" nao e um palindromo",palavra);
+        }
     }
-    public static boolean verificaPalindromo(String palavra, int n){
-       if (n==0){
-           return palavra.charAt(n) == palavra.charAt(palavra.length()-1-n);
+    public static Boolean verificaPalindromo(String palavra, int n){
+       if (n>0) {
+           if (palavra.charAt(n) == palavra.charAt(palavra.length() - 1 - n)) {
+               x++;
+           }
+           verificaPalindromo( palavra, n-1);
        }
-        return verificaPalindromo(palavra,n-1);
+        return x == palavra.length() - 1;
     }
 }
