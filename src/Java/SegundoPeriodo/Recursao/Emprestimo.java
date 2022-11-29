682f97e8cbc3617bf1def9d67a2670ecaebd707a
josevictoraraujorojas <kaneki1212112121@gmail.com>
2022-11-29 07:20:19 -0300

    Jose Victor

---
+++ src/Java/SegundoPeriodo/Recursao/Emprestimo.java
@@ -0,0 +1,17 @@
+package Java.SegundoPeriodo.Recursao;
+
+public class Emprestimo {
+    public static void main(String[] args) {
+        StringBuilder impressao = new StringBuilder();
+        System.out.println(calculo(impressao,2000/12, 0.04f,1,12));
+    }
+    public static StringBuilder calculo(StringBuilder impressao,float creditoIncial,float taxaDeJuros,int tempoInicial, int tempoFinal){
+        if (tempoInicial <= tempoFinal){
+            StringBuilder montante = new StringBuilder(String.valueOf((creditoIncial * Math.pow((1 + taxaDeJuros), tempoInicial))));
+            impressao.append(montante, 0, montante.indexOf(".")+3);
+            impressao.append("\n");
+            calculo(impressao,creditoIncial,taxaDeJuros,tempoInicial+1,tempoFinal);
+        }
+        return impressao;
+    }
+}
