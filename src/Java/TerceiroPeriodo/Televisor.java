4e587a22aa9c1084d6b69910e4faae7c4847b3eb
josevictoraraujorojas <kaneki1212112121@gmail.com>
2023-02-15 10:06:54 -0300

    Televisor

--- src/Java/TerceiroPeriodo/Televisor.java
+++ src/Java/TerceiroPeriodo/Televisor.java
@@ -1,50 +1,28 @@
 package Java.TerceiroPeriodo;
-import java.util.*;  
 
 public class Televisor {
 	
  public int volume;
  public int canal;
  
- public void aumentarVolume() {
-	 volume++;
+ public void aumentarVolume()
+ {
+	this.volume++;
  }
  
- public void reduzirVolume() {
-	 volume--;
+ public void reduzirVolume() 
+ {
+	 this.volume--;
  }
  
- public void trocarCanal() {
-	 Scanner ler = new Scanner(System.in);
-	 try
-	 {
-	 System.out.println("Qual o canal desejado?");
-	 canal=ler.nextInt();
-	 
-	 }catch(InputMismatchException e) {
-		 System.out.println("Resposta invalida");
-		 trocarCanal();
-	 }
+ public void trocarCanal(int canal) 
+ {
+	 this.canal=canal;
  }
  
- public void mostrar() {
-	 System.out.printf("\nVolume:%d\nCanal:%d\n",volume,canal);
+ public String mostrar() 
+ {
+	 return "\nVolume:"+volume+"\nCanal:"+canal+"\n";
  }
 
- public static void main(String[] args) {
- Televisor televisor = new Televisor();
- 
- televisor.canal=0;
- televisor.volume=30;
- televisor.mostrar();
-
- televisor.aumentarVolume();
- televisor.trocarCanal();
- televisor.mostrar();
- 
- televisor.reduzirVolume();
- televisor.mostrar();
- 
- }
- 
 }
