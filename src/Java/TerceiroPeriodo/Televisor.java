c6585f73c94f681bb13cc98433456c89cf4e476b
josevictoraraujorojas <kaneki1212112121@gmail.com>
2023-02-15 10:49:01 -0300

    Teevisor

--- src/Java/TerceiroPeriodo/Televisor.java
+++ src/Java/TerceiroPeriodo/Televisor.java
@@ -7,17 +7,23 @@ public class Televisor {
  
  public void aumentarVolume()
  {
+	 if(this.volume<100) {
 	this.volume++;
+	 }
  }
  
  public void reduzirVolume() 
  {
+	 if(this.volume>0){
 	 this.volume--;
+	 }
  }
  
  public void trocarCanal(int canal) 
  {
+	 if(this.canal<100 && this.canal>0) {
 	 this.canal=canal;
+	 }
  }
  
  public String mostrar() 
