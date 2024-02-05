c89ffc4c62ea9af9ecf34d4a6bc52afaa0b21f15
josevictoraraujorojas <kaneki1212112121@gmail.com>
2024-02-05 10:09:25 -0300

    05/02/24

--- src/Java/TerceiroPeriodo/POO/Televisor.java
+++ src/Java/TerceiroPeriodo/POO/Televisor.java
@@ -1,30 +1,13 @@
 package Java.TerceiroPeriodo.POO;
 
 public class Televisor {
-    private int volume;
-    private int canal;
+    public int volume;
+    public int canal;
 
     public Televisor(int volume, int canal) {
         this.volume = volume;
         this.canal = canal;
     }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public int getCanal() {
-        return canal;
-    }
-
-    public void setCanal(int canal) {
-        this.canal = canal;
-    }
-
     public void aumentarVolume(){
         this.volume++;
     }
