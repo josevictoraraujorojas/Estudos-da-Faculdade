6cfb6b852c2678d5cd5030289de8814fa73a717a
josevictoraraujorojas <kaneki1212112121@gmail.com>
2024-02-07 10:47:17 -0300

    07/02/24

--- src/Java/TerceiroPeriodo/EstruturaDeDados/ListaEstatica.java
+++ src/Java/TerceiroPeriodo/EstruturaDeDados/ListaEstatica.java
@@ -28,27 +28,30 @@ public class ListaEstatica {
         this.primeiro = 0;
         this.ultimo = 0;
     }
+    public void insereInicio (Object x) throws Exception {
+        if ( this . ultimo == this . item. length)
+            throw new Exception ( "Erro : A lista esta cheia" ) ;
+        for (int i = ultimo-1; i >=0; i--) {
+
+            this.item[i + 1] = this.item[i];
+        }
+        this.item[0] = x;
+        this.ultimo++;
+    }
+
     public void insereFinal (Object x) throws Exception {
-        if ( this . ultimo >= this . item. length)
-        throw new Exception ( "Erro : A lista esta cheia" ) ;
-       else {
+        if ( this . ultimo == this . item. length)
+            throw new Exception ( "Erro : A lista esta cheia" ) ;
+        else {
             this . item[ this . ultimo ] = x;
             this . ultimo = this . ultimo + 1;
         }
     }
-
-    public void insereInicio (Object x) {
-            for (int i = ultimo; i >=0; i--) {
-
-                this.item[i + 1] = this.item[i];
-            }
-            this.item[0] = x;
-            this.ultimo++;
-
-    }
-    public void insereNoMeio(int indice,Object x){
+    private void insereNoMeioIndice(int indice,Object x) throws Exception {
+        if ( this . ultimo == this . item. length)
+            throw new Exception ( "Erro : A lista esta cheia" ) ;
         if (indice<ultimo) {
-            for (int i = 8; i >= indice; i--) {
+            for (int i = ultimo-1; i >= indice; i--) {
 
                 this.item[i + 1] = this.item[i];
             }
@@ -56,11 +59,27 @@ public class ListaEstatica {
             this.ultimo++;
         }
     }
-    public void insereProximoIndice(ListaEstatica lista,int n1,int n2){
-        Object[] vetor = lista.getItem();
-        for (int i = 0; i < lista.getUltimo() ; i++) {
+
+    public void insereNoMeio(Object x) throws Exception {
+        int posicao=0;
+        if (!verificaListaDecrescente()){
+            ordenaDecrescente();
+        }
+        if ( this . ultimo == this . item. length)
+            throw new Exception ( "Erro : A lista esta cheia" ) ;
+            for (int i = 0; i < ultimo; i++) {
+                if ((int)this.item[i] < (int)x ) {
+                    posicao=i;
+                    break;
+                }
+            }
+            insereNoMeioIndice(posicao,x);
+    }
+    public void insereProximoIndice(int n1,int n2) throws Exception {
+        Object[] vetor = getItem();
+        for (int i = 0; i < getUltimo() ; i++) {
             if (vetor[i].equals(n1)) {
-                lista.insereNoMeio(i+1,n2);
+                insereNoMeioIndice(i+1,n2);
                 return;
             }
         }
@@ -75,25 +94,38 @@ public class ListaEstatica {
         }
         return count == ultimo - 1;
     }
+    public void ordenaDecrescente() {
+        Object[] vetor = getItem();
+        int auxiliar;
+        for (int j = 0; j < getUltimo() - 1; j++) {
+            for (int i = 0; i < getUltimo() - 1; i++) {
+                if ((int) vetor[i] < (int) vetor[i + 1]) {
+                    auxiliar=(int)vetor[i];
+                    vetor[i] = vetor[i+1];
+                    vetor[i+1]=auxiliar;
+                }
+            }
+        }
+    }
     public void procuraElementoRapido(Object x){
-        if (verificaListaDecrescente()){
+        if (!verificaListaDecrescente()){
+            ordenaDecrescente();
+        }
             Object[] vetor = getItem();
-            int metadedovetor=getUltimo()/2;
-            if ((int)x>(int)vetor[metadedovetor]){
-                for (int i = getUltimo()/2; i >= 0 ; i--) {
+            int metadedovetor = getUltimo() / 2;
+            if ((int) x > (int) vetor[metadedovetor]) {
+                for (int i = getUltimo() / 2; i >= 0; i--) {
                     if (vetor[i].equals(x)) {
                         System.out.println(i);
                     }
                 }
-            }else {
-                for (int i = getUltimo()/2; i < getUltimo() ; i++) {
+            } else {
+                for (int i = getUltimo() / 2; i < getUltimo(); i++) {
                     if (vetor[i].equals(x)) {
                         System.out.println(i);
                     }
                 }
             }
-
-        }
     }
 
     public int tamanho(){
--- src/Java/TerceiroPeriodo/EstruturaDeDados/Main.java
+++ src/Java/TerceiroPeriodo/EstruturaDeDados/Main.java
@@ -4,22 +4,17 @@ package Java.TerceiroPeriodo.EstruturaDeDados;
 public class Main {
     public static void main(String[] args) throws Exception {
         ListaEstatica lista = new ListaEstatica(10);
-        lista.insereInicio(11);
-        lista.insereInicio(12);
+        lista.insereInicio(90);
+        lista.insereInicio(10);
         lista.insereInicio(13);
-        lista.insereInicio(14);
+        lista.insereInicio(20);
         lista.insereInicio(15);
-        lista.insereInicio(16);
+        lista.insereInicio(10);
         System.out.println(lista);
-        lista.procuraElementoRapido(14);
+        lista.insereNoMeio(15);
+        System.out.println(lista);
 
-        lista.insereProximoIndice(lista,15,30);
-        System.out.println(lista);
-        lista.insereFinal(20);
-        lista.insereNoMeio(4,10);
-        System.out.println(lista);
-        System.out.println(lista.tamanho());
-        System.out.println(lista.validaPos(9));
+
 
 
 
