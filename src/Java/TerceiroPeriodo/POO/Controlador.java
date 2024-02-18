b88c8d3b239b03f4ddd366f58af2609555021ce0
josevictoraraujorojas <kaneki1212112121@gmail.com>
2024-02-18 10:11:53 -0300

    18/02/24

---
+++ src/Java/TerceiroPeriodo/POO/Controlador.java
@@ -0,0 +1,15 @@
+package Java.TerceiroPeriodo.POO;
+
+public interface Controlador {
+    void ligar();
+    void desligar();
+    void abrirMenu();
+    void fecharMenu();
+    void maisVolume();
+    void menosVolume();
+    void ligarMudo();
+    void desligarMudo();
+    void play();
+    void pause();
+
+}
---
+++ src/Java/TerceiroPeriodo/POO/MainControleRemoto.java
@@ -0,0 +1,11 @@
+package Java.TerceiroPeriodo.POO;
+
+public class MainControleRemoto {
+    public static void main(String[] args) {
+        ControleRemoto c =new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+    }
+}
