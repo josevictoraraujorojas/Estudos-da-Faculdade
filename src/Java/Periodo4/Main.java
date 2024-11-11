71df1beff9a4fb84255e1083233c3e356cf74d3d
josevictoraraujorojas <kaneki1212112121@gmail.com>
2024-11-11 16:34:18 -0300

    avl

--- src/Java/Periodo4/Main.java
+++ src/Java/Periodo4/Main.java
@@ -3,14 +3,18 @@ package Java.Periodo4;
 public class Main {
     public static void main(String[] args) {
         ArvoreAvl arvore = new ArvoreAvl();
-        arvore.inserir(10);
+        arvore.inserir(11);
         arvore.inserir(9);
         arvore.inserir(8);
         arvore.inserir(7);
 
         System.out.println("---------------------------");
 
-        arvore.retirar(10);
+        arvore.retirar(7);
 
+        arvore.inserir(10);
+
+        arvore.retirar(8);
     }
+
 }
